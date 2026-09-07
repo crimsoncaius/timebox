906ff0ef49a72b6169a5f5a3787155298db7f6bc
crimsoncaius <smileyfacechew@gmail.com>
2026-09-07 15:10:25 +0800

    prototype: capture mobile options and reliably launch selected variant

--- scripts/run-move-project-prototype.ps1
+++ scripts/run-move-project-prototype.ps1
@@ -3,5 +3,5 @@ $ErrorActionPreference = "Stop"
 & (Join-Path $PSScriptRoot "android-gradle.ps1") :app:installDebug
 if ($LASTEXITCODE -ne 0) { throw "Debug build or install failed." }
 $prototypeAdb = "C:\Users\Caius\AppData\Local\Android\Sdk\platform-tools\adb.exe"
-& $prototypeAdb shell am start -W -a android.intent.action.VIEW -d "timebox://prototype/move-project?variant=$Variant" com.timebox.android
+& $prototypeAdb shell am start -S -W -a android.intent.action.VIEW -d "timebox://prototype/move-project?variant=$Variant" -n com.timebox.android/.ui.battleplan.prototype.MoveProjectPrototypeActivity
 if ($LASTEXITCODE -ne 0) { throw "Could not launch prototype." }
